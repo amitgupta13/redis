1c980b54e95cdbe6ab32855f904acb20ffc73c77
Amit Gupta <amit.gupta@daffodilsw.com>
2019-07-24 23:17:03 +0530

    fc

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "895090814333-bm11k1l786d2jdsuiav9scnf4h85a209.apps.googleusercontent.com",
+  googleClientSecret: "nsTtMYH_r5YVoqhHc1i9xoJs",
+  mongoURI: "mongodb://localhost:27017/nodeadv",
+  cookieKey: "123123123"
+};
---
+++ test.js
@@ -0,0 +1,72 @@
+// const a = [{ a: 1, b: 2, c: 3, d: 4 }, { a: 1, b: 2, c: 3, d: 4 }];
+// const ob = array => {
+//   const obj = {};
+//   array.forEach((b, i) => {
+//     for (x in b) {
+//       obj[x + i] = b[x];
+//     }
+//   });
+//   console.log(obj);
+// };
+
+// ob(a);
+
+// class wo {
+//   abc = () => {
+//     console.log(this);
+//   };
+// }
+
+// const wla = new wo();
+
+// wla.abc();
+
+// const test = {
+//   busnumber: "5555",
+//   conductorid: "cond1",
+//   driverid: "driver1",
+//   fromdate: 1563518405000,
+//   todate: 1563518405000,
+//   shifttype: "morning",
+//   starttime: 1563492653213,
+//   endtime: 1563525053213,
+//   createdby: 57,
+//   depotId: 4398,
+//   agencyId: 4397,
+//   trips: [
+//     {
+//       tripType0: "Rounded",
+//       tsno: 1,
+//       rUp: "ABC",
+//       rUpNo: 1514,
+//       tround: 1
+//     }
+//   ]
+// };
+
+// function someFunction() {
+//   this.mixpanelService.ngOnInit(mixpannelProjectToken.project_token);
+//   this.mixpanelService.track("Duty Allocation", {
+//     cityName: this.etimsData.cityName,
+//     agencyName: this.etimsData.agencyName,
+//     createdBy: this.etimsData.createdBy,
+//     busnumber: this.dutyForm.value.busid,
+//     conductorid: this.dutyForm.value.conductorid,
+//     driverid: this.dutyForm.value.driverid,
+//     fromdate: this.dutyForm.value.startDate.getTime(),
+//     todate: this.dutyForm.value.endDate.getTime(),
+//     shifttype: this.dutyForm.value.shiftid,
+//     starttime: this.dutyForm.value.timeRange.startTime.getTime(),
+//     endtime: this.dutyForm.value.timeRange.endTime.getTime(),
+//     createdby: this.authService.userDetail.id,
+//     depotId: this.dutyForm.value.depotId,
+//     agencyId: this.dutyForm.value.agencyId,
+//     trips: this.dutyForm.value.trips
+//   });
+// }
+
+abba();
+
+function abba() {
+  console.log("abc");
+}
