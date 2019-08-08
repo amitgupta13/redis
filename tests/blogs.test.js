240ef2f54bf90b47a860bc02005e570cef718f8f
Amit Gupta <amit.gupta@daffodilsw.com>
2019-08-08 13:39:27 +0530

    nc

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -58,3 +58,34 @@ describe("When logged in", async () => {
     });
   });
 });
+
+describe("when not logged in", async () => {
+  test("user cannot create blog post", async () => {
+    const result = await page.evaluate(() => {
+      return fetch("/api/blogs", {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title: "My Title", content: "My Content" })
+      }).then(res => res.json());
+    });
+
+    expect(result).toEqual({ error: "You must log in!" });
+  });
+
+  test("user cannot get a list of posts", async () => {
+    const result = await page.evaluate(() => {
+      return fetch("/api/blogs", {
+        method: "GET",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(res => res.json());
+    });
+
+    expect(result).toEqual({ error: "You must log in!" });
+  });
+});
