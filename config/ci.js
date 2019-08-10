98a1103f12c4a300a347a64addc6b87c80792a78
Amit Gupta <amit.gupta@daffodilsw.com>
2019-08-10 19:31:42 +0530

    nc

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "895090814333-bm11k1l786d2jdsuiav9scnf4h85a209.apps.googleusercontent.com",
   googleClientSecret: "nsTtMYH_r5YVoqhHc1i9xoJs",
-  mongoURI: "mongodb://localhost:27017/nodeadv",
+  mongoURI: "mongodb://localhost:27017/blog_ci",
   cookieKey: "123123123",
   redisUrl: "127.0.0.1:6379"
 };
--- services/cache.js
+++ services/cache.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 const redis = require("redis");
 const util = require("util");
+const { redisUrl } = require("../config/keys");
 
-redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
-
 client.hget = util.promisify(client.hget);
-
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cache = function(options = {}) {
